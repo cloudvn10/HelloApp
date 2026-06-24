ee14f111d228b85933382b46be9d204ce8320628
cloudvn10 <19konom@gmail.com>
2026-06-24 17:40:41 +0300

    Update HelloAppApp.swift

--- HelloAppApp.swift
+++ HelloAppApp.swift
@@ -17,27 +17,23 @@ struct ContentView: View {
     }
 }
 
-// Делегат, который перехватывает data: и превращает в реальный файл
-class Coordinator: NSObject, WKNavigationDelegate {
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        
-        // Перехватываем ссылку data:
-        if let url = navigationAction.request.url, url.scheme == "data" {
-            // 1. Сохраняем содержимое data: во временный файл
-            if let data = try? Data(contentsOf: url) {
+// Делегат для перехвата сообщений от JavaScript
+class Coordinator: NSObject, WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        // Проверяем, что пришло сообщение с нужным именем
+        if message.name == "mobileConfigHandler", let xmlString = message.body as? String {
+            // 1. Превращаем полученный текст XML в данные
+            if let data = xmlString.data(using: .utf8) {
+                // 2. Сохраняем во временный файл
                 let tempFile = FileManager.default.temporaryDirectory.appendingPathComponent("ShadowVPN.mobileconfig")
                 try? data.write(to: tempFile)
                 
-                // 2. Открываем ЭТОТ файл в Safari (iOS сама выкинет окно профиля)
-                UIApplication.shared.open(tempFile, options: [:], completionHandler: nil)
+                // 3. Открываем файл в системе (появится окно установки профиля)
+                DispatchQueue.main.async {
+                    UIApplication.shared.open(tempFile, options: [:], completionHandler: nil)
+                }
             }
-            
-            // 3. Блокируем загрузку в WebView
-            decisionHandler(.cancel)
-            return
         }
-        
-        decisionHandler(.allow)
     }
 }
 
@@ -47,8 +43,12 @@ struct WebView: UIViewRepresentable {
     }
 
     func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
-        webView.navigationDelegate = context.coordinator
+        let config = WKWebViewConfiguration()
+        
+        // РЕГИСТРИРУЕМ ОБРАБОТЧИК СООБЩЕНИЙ ИЗ JS
+        config.userContentController.add(context.coordinator, name: "mobileConfigHandler")
+        
+        let webView = WKWebView(frame: .zero, configuration: config)
         
         if let url = Bundle.main.url(forResource: "index", withExtension: "html") {
             webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
