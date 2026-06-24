3d5f59d2cdd17cbf7ddda45872b233965ec98662
cloudvn10 <19konom@gmail.com>
2026-06-24 04:48:09 +0300

    Create HelloAppApp.swift

---
+++ HelloApp/HelloAppApp.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+@main
+struct HelloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
